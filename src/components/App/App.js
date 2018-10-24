4740f435e148f9bdb9eca0a67b4c941d0a2a6fe2
CoinBR <coinbr@gmail.com>
2018-10-24 12:55:29 -0300

    Add material-ui

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,31 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {MuiThemeProvider} from "material-ui/styles/index";
+import {AppBar, Toolbar, Typography} from "material-ui";
+import {createMuiTheme} from 'material-ui/styles';
+import red from 'material-ui/colors/red';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: red,
+    },
+});
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <MuiThemeProvider theme={theme}>
+                <React.Fragment>
+                    <AppBar position="static">
+                        <Toolbar>
+                            <Typography type="title" color="inherit">
+                                I ❤ TV
+                            </Typography>
+                        </Toolbar>
+                    </AppBar>
+                </React.Fragment>
+            </MuiThemeProvider>
+        );
+    }
 }
 
 export default App;
