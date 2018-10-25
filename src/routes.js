4f13a47823e43718787af730c700cc824d34374f
CoinBR <coinbr@gmail.com>
2018-10-25 17:07:54 -0300

    Declare the first routes in a routes.js file

---
+++ src/routes.js
@@ -0,0 +1,8 @@
+export const LANDING = '/';
+
+export const SIGN_IN = '/signin';
+export const SIGN_UP = '/signup';
+export const ACCOUNT = '/account';
+export const PASSWORD_FORGET = '/forgot-password';
+
+export const MY_MOVIES = '/my-movies';
